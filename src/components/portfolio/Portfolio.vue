1d215cab4a854403a5cd7312a50f3cb03f02e354
chenka <sarunyhot@gmail.com>
2017-08-29 20:54:04 +0700

    first commit

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import Stock from './Stock.vue'
+import store from '../../store/store'
+
+export default {
+  computed: {
+    ...mapGetters(
+      { stocks: 'stockPortfolio' }
+    )
+  },
+  components: {
+    appStock: Stock
+  }
+}
+</script>
+
+<style>
+
+</style>
